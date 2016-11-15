51aa4322d184c1e3c8375860c36473aae10417a0
kevinmendez <kevinmendez1007@gmail.com>
2016-11-14 18:48:23 -0600

    Cree la aplicación de la cafeteria y el modelo con las tablas requeridas

---
+++ cafeteria/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=25)
+    foto = models.ImageField(upload_to='photos/')
+    
+    def __str__(self):
+        return self.categoria
+
+class Producto(models.Model):
+    producto = models.CharField(max_length=200)
+    foto = models.ImageField(upload_to='photos/')
+    descripcion = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.producto
+    
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.TextField()
+    telefono = models.CharField(max_length=12)
+    correo = models.CharField(max_length=25, default='N/A')
+    usuario = models.CharField(max_length=20)
+    password = models.CharField(max_length = 20)
+    
+    def __str__(self):
+        return self.nombre
